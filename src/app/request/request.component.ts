d6157cada7b814a292d04bf6093c70d6aca1e353
Rommel-Hortel <hortelrommel@gmail.com>
2024-05-27 15:40:13 +0800

    SEARCH BAR KUWANG JAPON

---
+++ src/app/request/request.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-request',
+  standalone: true,
+  templateUrl: './request.component.html',
+  styleUrl: './request.component.css'
+})
+export class RequestComponent {
+
+}
